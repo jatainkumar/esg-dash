f7d392e974b6a64b4e03561caf9719f37866ea41
[Jatain Kumar] <codewithjatain@gmail.com>
2024-08-09 22:00:15 +0530

    app updated

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import geopandas as gpd
 import folium
-
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,7 +14,6 @@ import joblib
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import matplotlib.pyplot as plt
 from flask import Flask, url_for, render_template, request
 from forms import InputForm
@@ -31,18 +29,16 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(25, output_count)
         self.softmax = nn.Softmax()
         
-        
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         x = self.fc3(x)
         return self.softmax(x)
     
-input_dim = 21  # Change this to match the input features of your model
+input_dim = 21  # Adjusted to 20 features
 model1 = Net(input_dim, 2)
-model1.load_state_dict(torch.load('net_model_state_dict.pth'))
+model1.load_state_dict(torch.load('model.pth'))
 model1.eval()
-
 
 @app.route("/")
 @app.route("/home")
@@ -53,12 +49,35 @@ def home():
 def predict():
     form = InputForm()
     if form.validate_on_submit():
-        input_data = pd.DataFrame({
-            'national_inv': [form.national_inv.data],
-            'lead_time': [form.lead_time.data],
-            'forecast_3_month': [form.forecast_3_month.data]
-        })
-        input_tensor = torch.tensor(input_data.values, dtype=torch.float32)
+        input_data = {
+            'national_inv': form.national_inv.data,
+            'lead_time': form.lead_time.data,
+            'in_transit_qty': form.in_transit_qty.data,
+            'forecast_3_month': form.forecast_3_month.data,
+            'forecast_6_month': form.forecast_6_month.data if form.forecast_6_month.data else 0,
+            'forecast_9_month': form.forecast_9_month.data if form.forecast_9_month.data else 0,
+            'sales_1_month': form.sales_1_month.data if form.sales_1_month.data else 0,
+            'sales_3_month': form.sales_3_month.data if form.sales_3_month.data else 0,
+            'sales_6_month': form.sales_6_month.data if form.sales_6_month.data else 0,
+            'sales_9_month': form.sales_9_month.data if form.sales_9_month.data else 0,
+            'min_bank': form.min_bank.data if form.min_bank.data else 0,
+            'potential_issue': int(form.potential_issue.data) if form.potential_issue.data else 0,
+            'pieces_past_due': form.pieces_past_due.data if form.pieces_past_due.data else 0,
+            'perf_6_month_avg': form.perf_6_month_avg.data if form.perf_6_month_avg.data else 0,
+            'perf_12_month_avg': form.perf_12_month_avg.data if form.perf_12_month_avg.data else 0,
+            'local_bo_qty': form.local_bo_qty.data if form.local_bo_qty.data else 0,
+            'deck_risk': int(form.deck_risk.data) if form.deck_risk.data else 0,
+            'oe_constraint': int(form.oe_constraint.data) if form.oe_constraint.data else 0,
+            'ppap_risk': int(form.ppap_risk.data) if form.ppap_risk.data else 0,
+            'stop_auto_buy': int(form.stop_auto_buy.data) if form.stop_auto_buy.data else 0,
+            'rev_stop': int(form.rev_stop.data) if form.rev_stop.data else 0,
+            
+        }
+
+        # Convert input data to DataFrame
+        input_df = pd.DataFrame([input_data])
+        input_tensor = torch.tensor(input_df.values, dtype=torch.float32)
+        
         with torch.no_grad():
             output = model1(input_tensor)
         pytorch_pred = output.argmax(dim=1).item()
@@ -71,14 +90,12 @@ def predict():
             return render_template("result.html", title="Result", backorder=False)
     return render_template("predict.html", title="Predict", form=form)
 
-
 def calculate_optimal_path(num_trucks):
     # Mock data for companies
-    optimal_path= num_trucks
-    summary= "Optimal path calculated using given constraints."
+    optimal_path = num_trucks
+    summary = "Optimal path calculated using given constraints."
 
     return optimal_path, summary
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     IntegerField,
+    BooleanField,
     SubmitField
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 class InputForm(FlaskForm):
     national_inv = IntegerField(
@@ -14,9 +15,81 @@ class InputForm(FlaskForm):
         label="Lead Time",
         validators=[DataRequired()]
     )
+    in_transit_qty = IntegerField(
+        label="In Transit Qty",
+        validators=[DataRequired()]
+    )
     forecast_3_month = IntegerField(
         label="Forecast 3 Month",
         validators=[DataRequired()]
+    )
+    forecast_6_month = IntegerField(
+        label="Forecast 6 month",
+        validators=[Optional()]
+    )
+    forecast_9_month = IntegerField(
+        label="Forecast 9 month",
+        validators=[Optional()]
+    )
+    sales_1_month = IntegerField(
+        label="Sales 1 Month",
+        validators=[Optional()]
+    )
+    sales_3_month = IntegerField(
+        label="Sales 3 Month",
+        validators=[Optional()]
+    )
+    sales_6_month = IntegerField(
+        label="Sales 6 Month",
+        validators=[Optional()]
+    )
+    sales_9_month = IntegerField(
+        label="Sales 9 Month",
+        validators=[Optional()]
+    )
+    min_bank = IntegerField(
+        label="Minimum Bank",
+        validators=[Optional()]
+    )
+    potential_issue = IntegerField(
+        label="Potential issue",
+        validators=[Optional()]
+    )
+    pieces_past_due = IntegerField(
+        label="Pieces past due",
+        validators=[Optional()]
+    )
+    perf_6_month_avg = IntegerField(
+        label="Performance 6 Month Avg",
+        validators=[Optional()]
+    )
+    perf_12_month_avg = IntegerField(
+        label="Performance 12 Month Avg",
+        validators=[Optional()]
+    )
+    local_bo_qty = IntegerField(
+        label="Local Backorder Quantity",
+        validators=[Optional()]
+    )
+    deck_risk = IntegerField(
+        label="Deck Risk",
+        validators=[Optional()]
+    )
+    oe_constraint = IntegerField(
+        label="OE Constraint",
+        validators=[Optional()]
+    )
+    ppap_risk = IntegerField(
+        label="PPAP Risk",
+        validators=[Optional()]
+    )
+    stop_auto_buy = IntegerField(
+        label="Stop Auto Buy",
+        validators=[Optional()]
+    )
+    rev_stop = IntegerField(
+        label="Rev Stop",
+        validators=[Optional()]
     )
     num_trucks = IntegerField(
         label="Number of Trucks",
